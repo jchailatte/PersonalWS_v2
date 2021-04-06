711121bf71896a108e07a8b00707bf0740950cb9
Jonathan Chai <chaijw@usc.edu>
2021-04-05 23:40:43 -0700

    unlucky -.-

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withTM = require('next-transpile-modules')(['three', '@react-three/drei']);
 
+//refer to this once they update to @react-three/fiber and see what can be used
+//https://github.com/pmndrs/react-three-next
+//prob also install @react-three/postprocessing 
+//^refer to the package.json
+
 module.exports = withTM({
     webpack(config, { isServer }) {
         if (isServer) {
